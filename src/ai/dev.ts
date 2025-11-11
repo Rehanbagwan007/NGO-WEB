66f54a6f1a8cfb32896ea9fafa4728712db44130
Rehan Bagwan <rehanbagwan10000@gmail.com>
2025-11-11 14:17:55 +0000

    remove unccesarry tabs or pages from admin dashboard  , - gallery manage

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,8 +1,6 @@
 import { config } from 'dotenv';
 config();
 
-import '@/ai/flows/generate-fund-allocation-recommendations.ts';
-import '@/ai/flows/generate-student-progress-insights.ts';
 import '@/ai/flows/share-event-flow.ts';
 
     
